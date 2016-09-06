968b9542a3978423a57e9ef5d8865118c9c3bee7
Alexander Ustimenko <ustimenko.alexander@gmail.com>
2016-09-06 14:58:57 +0600

    Prepend functions to doctrine at configure stage

--- DependencyInjection/EkapustaDoctrineCustomTypesExtension.php
+++ DependencyInjection/EkapustaDoctrineCustomTypesExtension.php
@@ -9,6 +9,7 @@ use Symfony\Component\DependencyInjection\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Ekapusta\DoctrineCustomTypesBundle\ORM\Query\AST\Functions\FunctionRegistry;
 
 class EkapustaDoctrineCustomTypesExtension extends Extension implements PrependExtensionInterface
 {
@@ -20,10 +21,19 @@ class EkapustaDoctrineCustomTypesExtension extends Extension implements PrependE
             throw new RuntimeException('Doctrine bundle required!');
         }
 
+        $functions = new FunctionRegistry($container->getExtensionConfig('doctrine'));
+
         $container->prependExtensionConfig('doctrine', [
             'dbal' => [
                 'types' => (new TypeRegistry())->getDoctrineMapping(),
-            ]
+            ],
+            'orm' => [
+                'dql' => [
+                    'string_functions'   => $functions->getStringFunctions(),
+                    'numeric_functions'  => $functions->getNumericFunctions(),
+                    'datetime_functions' => $functions->getDatetimeFunctions(),
+                ],
+            ],
         ]);
     }
 
